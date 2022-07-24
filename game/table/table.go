e2100c60f3cb3650966f2969435392115683443a
Gleison Batista <gleisonbs@gmail.com>
2022-07-24 12:17:00 -0300

    feat: throw error for invalid seat number

--- game/table/table.go
+++ game/table/table.go
@@ -1,16 +1,25 @@
 package table
 
 import (
+	"errors"
+
 	"github.com/gleisonbs/poker-golang/game/deck"
 )
+
+var INVALID_SEAT_NUMBER_ERROR = errors.New("Seats must be a value between 2 and 12")
 
 type Table struct {
 	Seats int
 	Deck  deck.Deck
 }
 
-func New(seats int) Table {
+func New(seats int) (Table, error) {
+	table := Table{}
+	if seats < 2 || seats > 12 {
+		return table, INVALID_SEAT_NUMBER_ERROR
+	}
+
 	newDeck := deck.New()
-	table := Table{Seats: seats, Deck: newDeck}
-	return table
+	table = Table{Seats: seats, Deck: newDeck}
+	return table, nil
 }
