811f12cf0fe7630095f270f2fc6b3dc932194912
Gleison Batista <gleisonbs@gmail.com>
2022-07-24 12:17:16 -0300

    test: add table tests

---
+++ tests/table_test.go
@@ -0,0 +1,57 @@
+package table
+
+import (
+	"testing"
+
+	"github.com/gleisonbs/poker-golang/game/table"
+)
+
+func TestNewTable(t *testing.T) {
+	nSeats := 9
+	newTable, _ := table.New(nSeats)
+
+	if newTable.Seats != nSeats {
+		t.Errorf("Number of seats is %v, expected %v", newTable.Seats, nSeats)
+	}
+
+	if len(newTable.Deck.Cards) != 52 {
+		t.Errorf("Deck size is %v, expected %v", len(newTable.Deck.Cards), 52)
+	}
+
+	nSeats = 6
+	newTable, _ = table.New(nSeats)
+
+	if newTable.Seats != nSeats {
+		t.Errorf("Number of seats is %v, expected %v", newTable.Seats, nSeats)
+	}
+}
+
+func TestNewTableInvalidSeats(t *testing.T) {
+	nSeats := -2
+	_, err := table.New(nSeats)
+
+	if err != table.INVALID_SEAT_NUMBER_ERROR {
+		t.Errorf("Expected to throw error for invalid seat number %v, not throw", nSeats)
+	}
+
+	nSeats = 0
+	_, err = table.New(nSeats)
+
+	if err != table.INVALID_SEAT_NUMBER_ERROR {
+		t.Errorf("Expected to throw error for invalid seat number %v, not throw", nSeats)
+	}
+
+	nSeats = 1
+	_, err = table.New(nSeats)
+
+	if err != table.INVALID_SEAT_NUMBER_ERROR {
+		t.Errorf("Expected to throw error for invalid seat number %v, not throw", nSeats)
+	}
+
+	nSeats = 13
+	_, err = table.New(nSeats)
+
+	if err != table.INVALID_SEAT_NUMBER_ERROR {
+		t.Errorf("Expected to throw error for invalid seat number %v, not throw", nSeats)
+	}
+}
