c0326702c8d12cb67f5be25fe89bb49e8b506295
Gleison Batista <gleisonbs@gmail.com>
2022-10-09 14:41:45 -0300

    feat: add helper methods to table

--- game/table/table.go
+++ game/table/table.go
@@ -4,22 +4,92 @@ import (
 	"errors"
 
 	"github.com/gleisonbs/poker-golang/game/deck"
+	"github.com/gleisonbs/poker-golang/game/player"
 )
 
 var INVALID_SEAT_NUMBER_ERROR = errors.New("Seats must be a value between 2 and 12")
 
-type Table struct {
-	Seats int
-	Deck  deck.Deck
+type TableState struct {
+	BetValue  int
+	callValue int
 }
 
-func New(seats int) (Table, error) {
+type Blinds struct {
+	Small int
+	Big   int
+}
+
+type Table struct {
+	MaxSeats int
+	Players  []player.Player
+	Deck     deck.Deck
+	Pot      int
+	State    TableState
+	Dealer   int
+	Blinds   Blinds
+}
+
+func New(maxSeats int) (Table, error) {
 	table := Table{}
-	if seats < 2 || seats > 12 {
+	if maxSeats < 2 || maxSeats > 12 {
 		return table, INVALID_SEAT_NUMBER_ERROR
 	}
 
 	newDeck := deck.New()
-	table = Table{Seats: seats, Deck: newDeck}
+	table = Table{MaxSeats: maxSeats, Deck: newDeck}
 	return table, nil
+}
+
+func (t *Table) CollectBlinds() {
+	if len(t.Players) < 2 {
+		return
+	}
+
+	if len(t.Players) == 2 {
+		bigBlindPlayerIndex := (t.Dealer + 1) % len(t.Players)
+		t.Players[bigBlindPlayerIndex].Stack -= t.Blinds.Big
+		t.Pot += t.Blinds.Big
+		return
+	}
+
+	smallBlindPlayerIndex := (t.Dealer + 1) % len(t.Players)
+	bigBlindPlayerIndex := (t.Dealer + 2) % len(t.Players)
+	t.Players[smallBlindPlayerIndex].Stack -= t.Blinds.Small
+	t.Players[bigBlindPlayerIndex].Stack -= t.Blinds.Big
+	t.Pot += t.Blinds.Small
+	t.Pot += t.Blinds.Big
+}
+
+func (t *Table) DealStartingHand() {
+	for _, p := range t.Players {
+		playerHand := t.Deck.Deal(2)
+		p.Hand = playerHand
+	}
+}
+
+func (t *Table) SitPlayers() {
+	for i := 0; i < t.MaxSeats; i++ {
+		newPlayer := player.Player{Hand: nil, Stack: 2500}
+		t.Players = append(t.Players, newPlayer)
+	}
+}
+
+func (t *Table) UpdateDealerPosition() {
+	t.Dealer = (t.Dealer + 1) % len(t.Players)
+}
+
+func (t *Table) Start() {
+	t.Deck = deck.New()
+	t.Dealer = 0
+	t.Blinds = Blinds{Small: 5, Big: 10}
+	t.Deck.Shuffle()
+
+	// for _, p := range t.Players {
+	// 	playerAction, value := p.GetAction()
+	// 	switch playerAction {
+	// 	case player.Actions.Bet:
+	// 		t.State.BetValue += value
+	// 		t.Pot += value
+	// 	}
+	// }
 }
